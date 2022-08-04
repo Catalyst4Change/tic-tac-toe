bb3f5b3f4dbd6f3d906edabce3c055b0502ab1b3
Catalyst <103901063+Catalyst4Change@users.noreply.github.com>
2022-08-04 17:46:13 -0600

    Build out game functions

---
+++ game.js
@@ -0,0 +1,108 @@
+class ticTacToe {
+  constructor(tokenOne, tokenTwo) {
+    this.playerOneTurn = true;
+    this.turnsTaken = 0; //when gets to 9 game over
+    this.tokenPositions = {
+      1: "",
+      2: "",
+      3: "",
+      4: "",
+      5: "",
+      6: "",
+      7: "",
+      8: "",
+      9: ""
+    }
+  }
+
+  checkGridSpace(gridPosition) {
+    console.log("checkGridSpace rcvd:", gridPosition);
+    if (this.tokenPositions[gridPosition] === "") {
+      this.claimGridSpace(gridPosition);
+      console.log(gridPosition);
+    }
+  }
+
+  claimGridSpace(gridPosition) {
+    this.turnsTaken += 1
+    console.log('turns', this.turnsTaken);
+    if (this.playerOneTurn) {
+      this.tokenPositions[gridPosition] = playerOne.token;
+      this.playerOneTurn = false;
+    } else {
+      this.tokenPositions[gridPosition] = playerTwo.token;
+      this.playerOneTurn = true;
+    }
+    console.log(this.tokenPositions);
+    this.checkForWin();
+    this.checkForDraw();
+  }
+
+  checkForWin() {
+    if (this.tokenPositions[1] === this.tokenPositions[2] === this.tokenPositions[3]) {
+      this.someoneWon();
+    } else if (this.tokenPositions[4] === this.tokenPositions[5] === this.tokenPositions[6]) {
+      this.someoneWon();
+    } else if (this.tokenPositions[7] === this.tokenPositions[8] === this.tokenPositions[9]) {
+      this.someoneWon();
+    }
+  }
+
+  someoneWon() {
+    console.log('WIN!');
+    someoneWon = true;
+    this.startClearTimer()
+
+  }
+
+  checkForDraw(){
+    if (!someoneWon && this.turnsTaken === 9) {
+      console.log("DRAW!");
+      this.startClearTimer();
+    }
+  }
+
+  startClearTimer() {
+    var timeoutID = setTimeout(this.clearGameBoard(), [5000]);
+    console.log("timer started...");
+  }
+  
+  clearGameBoard() {
+    console.log("ding!");
+    for (let i = 0; i < this.tokenPositions.length; i++) {
+      this.tokenPositions[i] = "";
+      this.turnsTaken = 0;
+    }
+    console.log(this.tokenPositions);
+  }
+}
+
+
+
+/*
+
+win conditions
+123
+456
+789
+147
+258
+369
+159
+357
+
+know who's turn it is.
+display relevant data.
+
+player places toekn on board
+listen to each button 
+place icon in space via html
+change tokenPosition to 1/2
+
+if player1
+
+A way to keep track of which player’s turn it currently is
+A way to check the Game’s board data for win conditions
+A way to detect when a game is a draw (no one has won)
+A way to reset the Game’s board to begin a new game
+*/
---
+++ main.js
@@ -0,0 +1,32 @@
+// var spaceOne = document.getElementById('1');
+// var spaceTwo = document.getElementById('2');
+// var spaceThree = document.getElementById('3');
+// var spaceFour = document.getElementById('4');
+// var spaceFive = document.getElementById('5');
+// var spaceSix = document.getElementById('6');
+// var spaceSeven = document.getElementById('7');
+// var spaceEight = document.getElementById('8');
+// var spaceNine = document.getElementById('9');
+
+var gameBoard = document.querySelector('.game-board');
+var gridPosition = 0;
+var playerOne = new Player('🪚')
+var playerTwo = new Player('🪓')
+var someoneWon = false;
+var newGame = new ticTacToe(playerOne.token, playerTwo.token)
+console.log(newGame);
+
+
+gameBoard.addEventListener('click', intParser);
+
+function intParser(eventData) {
+  gridPosition = parseInt(eventData.target.id);
+  console.log(gridPosition);
+  newGame.checkGridSpace(gridPosition);
+}
+
+// newGame.checkGridSpace)
+
+// function showData(event) {
+//   console.log(event.path[0].id);
+// }
